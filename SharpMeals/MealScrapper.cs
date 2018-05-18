b97715dfd92d995ad79e1861ee74bc2448c21382
Onur <onur_sahin@chromium.org>
2018-05-18 23:08:52 +0300

    first commit Written MealScrapper class but didn't test the xpath query.(Only tested on an online tool)

---
+++ SharpMeals/MealScrapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using HtmlAgilityPack;
+
+namespace SharpMeals
+{
+    public class MealScrapper
+    {
+        public const string URL = "http://kafemud.bilkent.edu.tr/monu_eng.html";
+        public HtmlNode fixMenu { get; private set; }
+        public HtmlNode alternativeMenu { get; private set; }
+
+        public MealScrapper()
+        {
+            HtmlWeb web = new HtmlWeb();
+            var htmlDoc = web.Load(URL);
+            var mealContainers = htmlDoc.DocumentNode.SelectNodes("//table[@class='icerik']/tbody"); //To Do: test this xpath
+            fixMenu = mealContainers[1].SelectSingleNode("/tr[2]//table/tbody/tr");
+            alternativeMenu = mealContainers[2].SelectSingleNode("/tr[3]//table/tbody/tr");
+        }
+    }
+}
