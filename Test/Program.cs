46e9ce215ece33734170fbb81cf8b4f6cfbed413
zer0 <sahinonur2000@hotmail.com>
2018-05-22 22:20:59 +0300

    ...

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using SharpMeals;
 using HtmlAgilityPack;
 
@@ -11,15 +13,32 @@ namespace Test
 
         static void Main(string[] args)
         {
+            TestScrapper();
+            TestFactory();
+        }
+
+        private static void TestScrapper()
+        {
             var scrapper = new MealScrapper();
-            
+            //var fixLunchTds = scrapper.FixMenu[1].SelectNodes("./td");
+            //Console.WriteLine(fixLunchTds[0].SelectSingleNode(".//b").InnerText);
+
             using(var file = System.IO.File.Create("doc.html"))
             using(var writer = new System.IO.StreamWriter(file))
             {
-                foreach (var fix in scrapper.FixMenu)
-                    writer.Write(fix.InnerText);
-                foreach (var alternative in scrapper.AlternativeMenu)
-                    writer.Write(alternative.InnerText);
+                foreach(var meal in scrapper.FixMenu.Concat(scrapper.AlternativeMenu))
+                    writer.Write(meal.InnerText);   
+            }
+        }
+
+        private static void TestFactory()
+        {
+            var factory = new MealFactory();
+
+            using(var file = System.IO.File.Create("meals.txt"))
+            using(var writer = new System.IO.StreamWriter(file))
+            {
+                factory.mealList.ForEach( m => writer.Write(m) );
             }
         }
     }
